6650b6ae1131f445ded42cde03863762e3e3e1a0
alex <alexandre.fourcat@epitech.eu>
2019-10-27 09:16:09 +0100

    :art: Formatting with rustfmt.

--- build.rs
+++ build.rs
@@ -1,11 +1,11 @@
 // Copyright 2019 Thomas Nicollet
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,7 +15,6 @@
 extern crate protobuf_codegen_pure;
 
 fn main() {
-
     protobuf_codegen_pure::run(protobuf_codegen_pure::Args {
         out_dir: "src/protos",
         input: &["protos/component_schema.proto", "protos/entity.proto"],
@@ -24,5 +23,6 @@ fn main() {
             serde_derive: Some(true),
             ..Default::default()
         },
-    }).expect("protoc");
+    })
+    .expect("protoc");
 }
